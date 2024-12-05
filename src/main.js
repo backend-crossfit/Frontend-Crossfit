ab5ada79575549f1c1be715baf90d8764f46f76b
Andrew-css <salgado10442@gmail.com>
2024-12-05 13:42:23 -0500

    Cambios 5/12 v2

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import App from "./App.vue";
 import FloatingVue from 'floating-vue'
 import 'floating-vue/dist/style.css'
 
-axios.defaults.baseURL = 'http://localhost:4500/api/'
+axios.defaults.baseURL = 'https://backend-crossfit.onrender.com/api/'
 
 
 const pinia = createPinia();
